dffbcbedc77c0018c8e250f009a0ff977edc0795
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-08-16 17:23:31 +0900

    オブジェクトのトレースなどをするTraceServiceのひな形を作成

---
+++ src/Visibility/TraseService.cs
@@ -0,0 +1,22 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace RealVAC.Visibility;
+
+public sealed class TraceService
+{
+    public bool HasLineOfSight(
+        CCSPlayerController viewer,
+        CCSPlayerController target
+    )
+    {
+        var viewerPawn = viewer.PlayerPawn.Value;
+        var targetPawn = target.PlayerPawn.Value;
+
+        if (viewerPawn == null || targetPawn == null)
+            return true;
+
+        // TODO: traceray
+
+        return true;
+    }
+}
